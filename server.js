09ba69838541a01ed9653be13e2bc2d336f90353
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2016-10-24 14:36:43 -0400

    updated top-tracks

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ var events = require('events');
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('end', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+        .qs(args)
+        .end(function(response) {
+            if (response.ok) {
+                emitter.emit('end', response.body);
+            } else {
+                emitter.emit('error', response.code);
+            }
+        });
     return emitter;
 };
 
@@ -30,30 +29,33 @@ app.get('/search/:name', function(req, res) {
     var completed = 0;
     var relatedArtist = 0;
     searchReq.on('end', function(item) {
-
         var artist = item.artists.items[0];
         var searchRelate = getFromApi('artists/' + artist.id + '/related-artists', {});
-        searchRelate.on('end', function(item){
+        searchRelate.on('end', function(item) {
             relatedArtist = item.artists.length;
             artist.related = item.artists;
-            for (var i = 0; i < artist.related; i++) {
-                var topTrackRequests = getFromApi('artists/' + artists.id + '/top-tracks', {});
-                    topTrackRequests.on('end', function(item){
-                        artist.related[i].tracks = item.tracks; 
-                        completed ++;
-                        checkComplete(artist);
-                    });
-            
+            for (var i = 0; i < artist.related.length; i++) {
+                getRelated(artist.related[i],i);
             }
-                       
+
+            function getRelated(relatedArtist,i){
+                relReq = getFromApi('artists/'+relatedArtist.id+'/top-tracks',{'country': 'US'});
+                relReq.on('end',function(item){
+                    artist.related[i].tracks = item.tracks;
+                    completed++;
+                    checkComplete();
+                });
+            }
+
+            var checkComplete = function() {
+                if (completed === relatedArtist) {
+                    res.json(artist);
+                }
+            };
+
         });
-        
+
     });
-    var checkComplete = function(artist) {
-        if (completed === relatedArtist) {
-         res.json(artist);
-        }
-    }; 
 
     searchReq.on('error', function(code) {
         res.sendStatus(code);
